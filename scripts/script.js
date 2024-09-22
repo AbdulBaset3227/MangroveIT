bbcdc6a1421e83a8bc95e7834f8fed5cdb8c4bd8
Abdul Baset <148635255+AbdulBaset3227@users.noreply.github.com>
2024-09-22 11:47:38 +0600

    Create script.js

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+        // JavaScript for hamburger menu toggle
+        document.getElementById('hamburger').addEventListener('click', function() {
+            const mobileMenu = document.getElementById('mobileMenu');
+            mobileMenu.classList.toggle('active');
+        });
